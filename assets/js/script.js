7d8737e6f2b0e84e34a24b2faf268546014faa8b
carsonadair <carsonadairphoto@gmail.com>
2023-01-09 15:55:42 -0500

    pseudo code js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/*
+
+Create input form
+Save input form information to local storage
+Show previous searches from local storage onto page
+Make previous searches clickable
+Add weather api
+User weather api to fetch current weather for selected city
+User weather api to fetch 5 day forecast for each city
+Add display icons for different kinds of weather that show up for each kind of weather.
+
+*/
